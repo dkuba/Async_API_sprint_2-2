a22f4b6cf51aca3f15be14179566c6b3cee0f4fa
Victor Merkushov <merkushov.victor@gmail.com>
2021-12-10 15:05:04 +0300

    Функциональные тесты для Жанров (gh-8)

---
+++ tests/funtional/src/test_genres_list.py
@@ -0,0 +1,31 @@
+from typing import Callable
+
+import pytest
+
+
+@pytest.mark.usefixtures("data_setup")
+@pytest.mark.data_setup_params("genres", "genres_list.json")
+@pytest.mark.asyncio
+class TestGenreList:
+    async def test_success(self, make_get_request: Callable):
+        response = await make_get_request("/genre", {})
+
+        assert response.status == 200
+        assert response.body[0].get("name", None)
+        assert len(response.body) == 15
+
+    async def test_check_paging_out_of_range(self, make_get_request: Callable):
+        response = await make_get_request(
+            "/genre", {"page[number]": 2, "page[size]": 15}
+        )
+
+        assert response.status == 200
+        assert len(response.body) == 0
+
+    async def test_check_paging_page_size(self, make_get_request: Callable):
+        response = await make_get_request(
+            "/genre", {"page[number]": 4, "page[size]": 3}
+        )
+
+        assert response.status == 200
+        assert len(response.body) == 3
